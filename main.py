0cccb000b579117bea77d723417621d8d776842a
Pancakeloki <dodoouasli@gmail.com>
2026-02-26 19:47:44 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter your num here: "))
+num2=int(input("Enter your 2nd num here: "))
+
+if num1 is num2:
+    print("your 2 numbers are the same")
+else:
+    print("they are not the same")
